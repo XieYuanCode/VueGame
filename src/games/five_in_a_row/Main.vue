9748aac9994e1e26655f903030eec87d5bb27469
谢渊 <xieyuan@agree.com.cn>
2022-09-20 18:28:55 +0800

    init

--- src/games/five_in_a_row/Main.vue
+++ src/games/five_in_a_row/Main.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>五子棋</div>
+  <div class="five-in-a-row w-full h-full flex justify-center items-center flex-col">
+    <div class="game-container w-full h-auto flex justify-around items-center">922
+      <div class="white-chest-box w-10 h-10 bg-black mt-60"></div>
+      <div class="checkerboard bg-gray-50 p-5 rounded-lg shadow-xl w-auto h-auto grid">
+        <div class="checkerboard-item bg-red-50 border border-gray-400" v-for="item in 196"></div>
+      </div>
+      <div class="black-chest-box w-10 h-10 bg-black mt-60"></div>
+    </div>
+    <div class="command-panel mt-5">
+      <n-button>开始</n-button>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup>import { useGameManager } from '../../../lib/store';
+<script lang="ts" setup>
+import { NButton } from "naive-ui"
 
-const gameManager = useGameManager()
-const game = gameManager.games.find(gameItem => gameItem.name === "五子棋")
-
-await game?.start()
 </script>
+
+<style scoped>
+.checkerboard {
+  grid-template-columns: repeat(14, 40px);
+  grid-template-rows: repeat(14, 40px);
+}
+</style>
